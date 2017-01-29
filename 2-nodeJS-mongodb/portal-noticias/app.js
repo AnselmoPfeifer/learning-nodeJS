4ff0b71d5e4c0f8310e02550277c4bf54b7a2175
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-29 00:13:49 -0300

    Ajuste de rotas, e separando os modulos

--- 2-nodeJS-mongodb/portal-noticias/app.js
+++ 2-nodeJS-mongodb/portal-noticias/app.js
@@ -3,17 +3,13 @@
  */
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
 
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+var rotaHome = require('./app/routes/home')(app);
 
-var rotaFormularioNoticias = require('./app/routes/formulario_inclusao_noticias');
-rotaFormularioNoticias(app);
+var rotaFormularioNoticias = require('./app/routes/formulario_inclusao_noticias')(app);
 
-var rotaTecnologia = require('./app/routes/tecnologia');
-rotaTecnologia(app)
+var rotaTecnologia = require('./app/routes/tecnologia')(app)
 
 app.listen(3000, function () {
     console.log('Server com express!')
