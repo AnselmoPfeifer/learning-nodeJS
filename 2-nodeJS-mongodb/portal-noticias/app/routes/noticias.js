4bfefc79eafc28bc296787c88b4d70260ab22ab9
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-29 01:17:37 -0300

    Listando as noticias do banco de dados mysql

--- 2-nodeJS-mongodb/portal-noticias/app/routes/noticias.js
+++ 2-nodeJS-mongodb/portal-noticias/app/routes/noticias.js
@@ -1,8 +1,30 @@
 /**
  * Created by anselmo on 28/01/17.
  */
+
+// module.exports = function (app) {
+//     app.get('/noticias', function (req, res) {
+//         res.render('noticias/noticias');
+//     });
+// }
+
 module.exports = function (app) {
     app.get('/noticias', function (req, res) {
-        res.render('noticias/noticias');
+        var mysql = require('mysql');
+        var conection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '123',
+            database: 'curso_nodejs'
+        });
+
+        conection.query('select * from noticias', function (error, result){
+            // retorna um json
+            //res.send(result);
+
+            // retorna um json e na view (ejs)
+            res.render('noticias/noticias', {noticias: result});
+        });
     });
-}
+
+};
