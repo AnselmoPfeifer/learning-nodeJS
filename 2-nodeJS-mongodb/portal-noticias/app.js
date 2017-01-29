69b9abab578ad005198288f6586b1823e6ed2ebf
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-28 23:51:42 -0300

    Separando as configs do server!

--- 2-nodeJS-mongodb/portal-noticias/app.js
+++ 2-nodeJS-mongodb/portal-noticias/app.js
@@ -2,11 +2,7 @@
  * Created by anselmo on 28/01/17.
  */
 
-var msg = require('./modulos/test');
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
+var app = require('./config/server');
 
 app.get('/tecnologia', function (req, res) {
     res.render('sessao/tecnologia');
@@ -26,5 +22,4 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
     console.log('Server com express!')
-    console.log(msg())
 })
---
+++ 2-nodeJS-mongodb/portal-noticias/config/server.js
@@ -0,0 +1,8 @@
+/**
+ * Created by anselmo on 28/01/17.
+ */
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+module.exports = app;
