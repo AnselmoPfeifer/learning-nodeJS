16789b3e1ebab079293330226158b9d52b2532e6
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-29 17:50:59 -0300

    Ajuste na pagina que exibe as noticias

--- 2-nodeJS-mongodb/portal-noticias/app/routes/noticias.js
+++ 2-nodeJS-mongodb/portal-noticias/app/routes/noticias.js
@@ -5,7 +5,7 @@
 module.exports = function(app) {
     app.get('/noticias', function(req, res) {
         var conection = app.config.conectionDb();
-        conection.query('select * from noticias', function(error, result) {
+        conection.query('select id_noticia, titulo from noticias', function(error, result) {
             res.render('noticias/noticias', { noticias: result });
         });
     });
