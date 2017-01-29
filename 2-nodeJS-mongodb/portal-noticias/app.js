7761308bc974e653fc6be1bf1ed2d2b7dc2be0e3
AnselmoPfeifer <anspfeifer@gmail.com>
2017-01-29 17:13:58 -0300

    Add as rotas inicializacao do server com Consign.

--- 2-nodeJS-mongodb/portal-noticias/app.js
+++ 2-nodeJS-mongodb/portal-noticias/app.js
@@ -2,20 +2,16 @@
  * Created by anselmo on 28/01/17.
  */
 var app = require('./config/server');
-
-var rotaNoticias = require('./app/routes/noticias')(app);
-
-var rotaHome = require('./app/routes/home')(app);
-
-var rotaFormularioNoticias = require('./app/routes/formulario_inclusao_noticias')(app);
-
-var rotaTecnologia = require('./app/routes/tecnologia')(app)
+/*
+  Usando o consgin ele identifica as routas existentes.
+  var rotaNoticias = require('./app/routes/noticias')(app);
+  var rotaHome = require('./app/routes/home')(app);
+  var rotaFormularioNoticias = require('./app/routes/formulario_inclusao_noticias')(app);
+  var rotaTecnologia = require('./app/routes/tecnologia')(app)
+*/
 
 app.listen(3000, function() {
     console.log('Server com express!')
-    if app == 1 {
-        console.console.log();
-    }
 })
 
 // TODO fazer mais
--- 2-nodeJS-mongodb/portal-noticias/config/server.js
+++ 2-nodeJS-mongodb/portal-noticias/config/server.js
@@ -2,9 +2,11 @@
  * Created by anselmo on 28/01/17.
  */
 var express = require('express');
+var consign = require('consign');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
+consign().include('app/routes').into(app);
 module.exports = app;
